73c8134a5a4b6bd377fa69b1f1af4aa9ae75d22e
Frederic Mbea <fredericmbea@gmail.com>
2019-11-16 17:52:58 +0000

    Seeded the database

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 
 gem 'devise'
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+20.times do |t|
+    User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, level: "Senior", github_link: "https://github.com/frederic92", password: "frederic")
+end
