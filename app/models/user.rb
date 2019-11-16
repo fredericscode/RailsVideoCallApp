22c2ba0e3dc39bc46c8d2fe971fdecc0cac87775
Frederic Mbea <fredericmbea@gmail.com>
2019-11-16 20:17:43 +0000

    Added the state field to the users table Added font awesome cdn in the layout page The camera icon is now showing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  enum state: { available: 0, busy: 1 }   
 end
